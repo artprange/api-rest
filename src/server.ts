65f7943ea0bd9730121e3ede099c1ec6c7607a20
artprange <arthur.prange@gmail.com>
2024-08-28 17:30:29 -0300

    api setup

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import fastify from 'fastify'
+
+const app = fastify()
+
+app.get('/helloo', ()=>{
+    return 'Hellooooo!'
+})
+
+app.listen({
+    port: 3333
+}).then(() =>{
+    console.log('http server running on port 3333' )
+})
