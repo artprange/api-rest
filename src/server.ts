d1996220a0f0d2536d6283a27b973bc52486230a
artprange <arthur.prange@gmail.com>
2024-08-28 17:46:46 -0300

    linitng

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,14 @@ import fastify from 'fastify'
 
 const app = fastify()
 
-app.get('/helloo', ()=>{
-    return 'Hellooooo!'
+app.get('/helloo', () => {
+  return 'Hellooooo!'
 })
 
-app.listen({
-    port: 3333
-}).then(() =>{
-    console.log('http server running on port 3333' )
-})
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('http server running on port 3333')
+  })
