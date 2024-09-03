3835164d008f597711db39787588b638ce0b8b73
artprange <arthur.prange@gmail.com>
2024-09-03 17:38:57 -0300

    listing transactions

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -3,6 +3,28 @@ import { z } from 'zod'
 import { knex } from '../database'
 
 export async function transactionsRoutes(app: FastifyInstance) {
+  app.get('/', async () => {
+    const transactions = await knex('transactions').select()
+
+    return {
+      transactions,
+    }
+  })
+
+  app.get('/:id', async (request) => {
+    const getTransactionParmsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = getTransactionParmsSchema.parse(request.params)
+
+    const transaction = await knex('transactions').where('id', id).first()
+
+    return {
+      transaction,
+    }
+  })
+
   app.post('/', async (request, reply) => {
     const createTransactionBodySchema = z.object({
       title: z.string(),
