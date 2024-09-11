a34e1e6e983eb7bf422e55152f2ee7e2732a4241
artprange <arthur.prange@gmail.com>
2024-09-11 15:40:32 -0300

    test database

---
+++ dud.js
@@ -0,0 +1,14 @@
+/*
+    o que senti agora, é o que eu acho que é?
+
+    digo, que sou capaz e que CONSEGUI o realmente vim perseguindo há tanto tempo?
+
+    esse sentimento de dever cumprido?
+
+    conversamos bastante e voce sabe o que quero. 
+
+    não queo me precipitar, mas quero que a resposta da minha pergunta seja sim. eu mereço isso. 
+
+    obrigado.
+
+*/
--- test/transaction.spec.ts
+++ test/transaction.spec.ts
@@ -1,4 +1,5 @@
-import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import { afterAll, beforeAll, beforeEach, describe, expect, it } from 'vitest'
+import { execSync } from 'node:child_process'
 import request from 'supertest'
 import { app } from '../src/app'
 
@@ -6,8 +7,14 @@ describe('Transactions routes', () => {
   beforeAll(async () => {
     await app.ready()
   })
+
   afterAll(async () => {
     await app.close()
+  })
+
+  beforeEach(() => {
+    execSync('npm run knex migrate:rollback --all')
+    execSync('npm run knex migrate:latest')
   })
 
   it('should be able to create a new transaction', async () => {
@@ -33,24 +40,23 @@ describe('Transactions routes', () => {
     // Obtendo os cookies da resposta
     const cookies = createTransactionResponse.get('Set-Cookie')
 
-    // Verificando se os cookies estão definidos
-    console.log('Cookies:', cookies)
-
-    if (cookies) {
-      // Somente fazer a requisição se os cookies estiverem definidos
-      const listTransactionsResponse = await request(app.server)
-        .get('/transactions')
-        .set('Cookie', cookies)
-        .expect(200)
-
-      expect(listTransactionsResponse.body.transactions).toEqual([
-        expect.objectContaining({
-          title: 'New transaction',
-          amount: 5000,
-        }),
-      ])
-    } else {
-      console.error('No cookies returned from the transaction creation request')
+    // Verificação adicional para garantir que os cookies não sejam indefinidos
+    if (!cookies || cookies.length === 0) {
+      throw new Error(
+        'No cookies returned from the transaction creation request',
+      )
     }
+
+    const listTransactionsResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies) // Garantindo que o cookies seja um array de strings
+      .expect(200)
+
+    expect(listTransactionsResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: 'New transaction',
+        amount: 5000,
+      }),
+    ])
   })
 })
