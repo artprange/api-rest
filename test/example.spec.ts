c26c428165b8ba18f53196279b06b832c9678709
artprange <arthur.prange@gmail.com>
2024-09-11 15:12:17 -0300

    transaction test

--- test/example.spec.ts
+++ test/example.spec.ts
@@ -1,7 +1,23 @@
-import { expect, test } from 'vitest'
+import { afterAll, beforeAll, test } from 'vitest'
+import request from 'supertest'
 
-test('o usuário consegue criar uma nova transação', () => {
-  const respnseStatusCode = 201
+import { app } from '../src/app'
 
-  expect(respnseStatusCode).toEqual(201)
+beforeAll(async () => {
+  await app.ready()
+})
+
+afterAll(async () => {
+  await app.close()
+})
+
+test('use can create a new transaction', async () => {
+  await request(app.server)
+    .post('/transactions')
+    .send({
+      title: 'New transaction',
+      amount: 5000,
+      type: 'credit',
+    })
+    .expect(201)
 })
